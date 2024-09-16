b2837d7d6a2ae07197088e7c4ddc7a91082a5a3f
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2024-09-16 14:03:04 +0200

    feat(mux): create webserver

--- main.go
+++ main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"context"
+	"github.com/gorilla/mux"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
 
+func main() {
+	r := mux.NewRouter()
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         ":80",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	slog.Info("Starting...")
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			slog.Error(err.Error())
+		}
+	}()
+
+	slog.Info("Started")
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+	err := srv.Shutdown(ctx)
+	if err != nil {
+		panic(err)
+	}
+	slog.Info("Shutting down")
+	os.Exit(0)
 }
